9ea6e5c469fcf0dda6198bc2abfa97c8c874dcdd
kalaikannanJK <47594774+kalaikannanJK@users.noreply.github.com>
2019-03-25 16:05:40 +0530

    Create even int.c

---
+++ even int.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+void main
+
+{
+
+int i;
+
+printf("\n the even numbers are:");
+
+for(i=10;i<=20;i++)
+
+{
+
+if(i%2==0)
+
+printf("%d",i);
+
+printf("\n");
+
+}
+
+ 
+
+return 0;
+
+}
